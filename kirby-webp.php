eacafe599df7808feb514fd35a1ae4c2319f7b60
Martin Folkers <hello@twobrain.io>
2018-02-28 22:09:57 +0100

    Fixing Composer autoload

--- kirby-webp.php
+++ kirby-webp.php
@@ -1,19 +1,19 @@
 <?php
 
 /**
- * Kirby WebP - INSERT DESCRIPTION
+ * Kirby WebP - WebP generation for Kirby
  *
  * @package   Kirby CMS
  * @author    S1SYPHOS <hello@twobrain.io>
  * @link      http://twobrain.io
- * @version   0.1.0
+ * @version   0.3.2
  * @license   MIT
  */
 
 if(!c::get('plugin.kirby-webp')) return;
 
 // Initialising composer's autoloader
-require_once  __DIR__ . DS . 'vendor' . DS . 'autoload.php';
+require_once kirby()->roots()->index() . DS . 'vendor' . DS . 'autoload.php';
 
 // Loading settings & core
 include_once __DIR__ . DS . 'core' . DS . 'generate_webp.php';
