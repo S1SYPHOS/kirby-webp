1236b5761abfb941d0546acfdd6552a747a593f2
M. Folkers <hello@twobrain.io>
2018-03-16 12:17:54 +0100

    Applying PSR2 coding style

--- core/generate_webp.php
+++ core/generate_webp.php
@@ -12,42 +12,42 @@
  *
  */
 
-class Convert {
+class Convert
+{
+    protected $quality;
+    protected $strip;
+    protected $serve;
+    protected $log;
+    protected $converters;
 
-  private $quality;
-  private $strip;
-  private $serve;
-  private $log;
-  private $converters;
-
-  public function __construct() {
-    $this->quality = c::get('plugin.kirby-webp.quality', 90);
-    $this->strip = c::get('plugin.kirby-webp.stripMetadata', TRUE);
-    $this->serve = c::get('plugin.kirby-webp.converters', TRUE);
-    $this->log = c::get('plugin.kirby-webp.serveOriginalOnFail', TRUE);
-    $this->converters = c::get('plugin.kirby-webp.converters', ['gd', 'cwebp']);
-  }
-  
-  public function generateWebP($file) {
-
-    try {
-      // Checking file type since only images are processed
-      if ($file->type() == 'image') {
-
-        // WebPConvert options
-        $input   = $file->dir() . '/' . $file->filename();
-        $output  = $file->dir() . '/' . $file->name() . '.webp';
-
-        WebPConvert::$serve_original_image_on_fail = $this->serve;
-        WebPConvert::$serve_converted_image = $this->log;
-        WebPConvert::set_preferred_converters($this->converters);
-        // WebPConvert::$debug = FALSE;
-
-        // Generating WebP image & placing it alongside the original version
-        WebPConvert::convert($input, $output, $this->quality, $this->strip);
-      }
-    } catch (Exception $e) {
-      return response::error($e->getMessage());
+    public function __construct()
+    {
+        $this->quality = c::get('plugin.kirby-webp.quality', 90);
+        $this->strip = c::get('plugin.kirby-webp.stripMetadata', true);
+        $this->serve = c::get('plugin.kirby-webp.converters', true);
+        $this->log = c::get('plugin.kirby-webp.serveOriginalOnFail', true);
+        $this->converters = c::get('plugin.kirby-webp.converters', ['gd', 'cwebp']);
     }
-  }
+
+    public function generateWebP($file)
+    {
+        try {
+            // Checking file type since only images are processed
+            if ($file->type() == 'image') {
+                // WebPConvert options
+                $input   = $file->dir() . '/' . $file->filename();
+                $output  = $file->dir() . '/' . $file->name() . '.webp';
+
+                WebPConvert::$serve_original_image_on_fail = $this->serve;
+                WebPConvert::$serve_converted_image = $this->log;
+                WebPConvert::setPreferredConverters($this->converters);
+                // WebPConvert::$debug = FALSE;
+
+                // Generating WebP image & placing it alongside the original version
+                WebPConvert::convert($input, $output, $this->quality, $this->strip);
+            }
+        } catch (Exception $e) {
+            return response::error($e->getMessage());
+        }
+    }
 }
--- kirby-webp.php
+++ kirby-webp.php
@@ -10,21 +10,18 @@
  * @license   MIT
  */
 
-if(!c::get('plugin.kirby-webp')) return;
+if (c::get('plugin.kirby-webp', false)) {
+    // Initialising composer's autoloader
+    require_once __DIR__ . DS . 'vendor' . DS . 'autoload.php';
 
-// Initialising composer's autoloader
-require_once __DIR__ . DS . 'vendor' . DS . 'autoload.php';
+    // Loading settings & core
+    require_once __DIR__ . DS . 'core' . DS . 'generate_webp.php';
 
-// Loading settings & core
-function webp() {
-  require_once __DIR__ . DS . 'core' . DS . 'generate_webp.php';
-  return new Kirby\Plugins\WebP\Convert;
-}
+    $hooks = c::get('plugin.kirby-webp.hooks', ['upload']);
 
-$hooks = c::get('plugin.kirby-webp.hooks', ['upload']);
-
-foreach ($hooks as $hook) {
-  kirby()->hook('panel.file.' . $hook, function ($file) {
-    webp()->generateWebP($file);
-  });
+    foreach ($hooks as $hook) {
+        kirby()->hook('panel.file.' . $hook, function ($file) {
+            (new Kirby\Plugins\WebP\Convert)->generateWebP($file);
+        });
+    }
 }
